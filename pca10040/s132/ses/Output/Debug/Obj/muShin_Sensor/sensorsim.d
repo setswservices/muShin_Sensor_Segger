032a806752b3bc68b9b5adf7b98b2dbbe0db5b1d
loch-cp <jbahr@set-software-services.com>
2020-12-29 13:40:10 -0700

    Get rid of non-source

--- pca10040/s132/ses/Output/Debug/Obj/muShin_Sensor/sensorsim.d
+++
@@ -1,5 +0,0 @@
-Output/Debug/Obj/muShin_Sensor/sensorsim.o: \
- D:\Projects\muShin\nRF5_SDK_15.3.0_59ac345\components\libraries\sensorsim\sensorsim.c \
- D:\Projects\muShin\nRF5_SDK_15.3.0_59ac345\components\libraries\sensorsim\sensorsim.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.32a/include/stdint.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.32a/include/stdbool.h
--- pca10040/s132/ses/Output/Debug/Obj/muShin_Sensor/ses_startup_nrf_common.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/muShin_Sensor/ses_startup_nrf_common.o: \
- D:\Projects\muShin\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\ses_startup_nrf_common.s
